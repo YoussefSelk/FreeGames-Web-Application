2c4226ccaa1d206e064029756542cc6bea6cea24
Youssef Selk <113248214+YoussefSelk@users.noreply.github.com>
2024-06-26 18:38:31 +0100

    Add More Information

--- assets/js/API/getApiKey.js
+++ assets/js/API/getApiKey.js
@@ -1,6 +1,6 @@
 // This file exports the RapidAPI key for accessing the Free-to-Play Games Database API.
 
-const RapidAPI = "";
+const RapidAPI = "ADD_YOUR_KEY_HERE";
 
 // Exporting the API key as default for use in other modules.
 export default RapidAPI;
